37f8eb740ccd5580154094ac844249e26c875e5b
Don Bosco van Hoi <donbosco.rulz@gmail.com>
2016-04-28 20:25:21 +0200

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+set -a
+source .env
+
+docker rm -f `docker ps -aq -f name=${PROJECT_NAME}_*`
+docker rm -v $(docker ps -a -q -f status=exited)
+docker images -q --filter dangling=true | xargs docker rmi
