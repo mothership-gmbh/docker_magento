7fe06e6db89b452ce8d52936f91d4ac8d9ea7740
Don Bosco van Hoi <vanhoi@mothership.de>
2016-06-20 12:28:09 +0200

    Added Kibana

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@
 
 docker rm -f `docker ps -aq -f name=${PROJECT_NAME}_*`
 
-export DOCKER_IP=$(dlite ip)
 
 
 set -a
@@ -12,6 +11,7 @@ source .env
 # create the subdirectories for the volumes
 mkdir -p ${PROJECT_VOlUME}/htdocs
 mkdir -p ${PROJECT_VOlUME}/db
+mkdir -p ${PROJECT_VOlUME}/elasticsearch
 mkdir -p ${PROJECT_VOlUME}/elasticsearch/data
 mkdir -p ${PROJECT_VOlUME}/elasticsearch/config
 mkdir -p ${PROJECT_VOlUME}/apache2
@@ -25,11 +25,10 @@ if [ ! -f ${PROJECT_VOlUME}/home/.bash_history ]; then
 fi
 
 cp ./elasticsearch/config/elasticsearch.yml ${PROJECT_VOlUME}/elasticsearch/config/elasticsearch.yml
-cp ./apache/host.conf <> ${PROJECT_VOlUME}/elasticsearch/config/elasticsearch.yml
 
 envsubst < ./apache/host.conf > ${PROJECT_VOlUME}/apache2/${PROJECT_NAME}.conf
 
 
-cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p "${PROJECT_NAME}" up
+cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p "${PROJECT_NAME}" --verbose up
 #cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p "${PROJECT_NAME}" up --build
 #cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p "${PROJECT_NAME}" up -d
--- sync.sh
+++ sync.sh
@@ -8,6 +8,7 @@ rm -rf ${PROJECT_VOlUME}/htdocs/.modman
 rm -rf ${PROJECT_VOlUME}/htdocs/www
 rm -rf ${PROJECT_VOlUME}/htdocs/vendor
 
+
 rsync -rv --exclude=.git ${PROJECT_SOURCE}/.modman/ ${PROJECT_VOlUME}/htdocs/.modman
 rsync -rv --exclude=.git ${PROJECT_SOURCE}/www/ /${PROJECT_VOlUME}/htdocs/www
 rsync -rv --exclude=.git ${PROJECT_SOURCE}/vendor/ ${PROJECT_VOlUME}/htdocs/vendor
