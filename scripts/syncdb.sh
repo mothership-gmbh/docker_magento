96d096bbf9eccc0f59cd341155b7ede86e29b634
Don Bosco van Hoi <vanhoi@mothership.de>
2016-12-07 13:24:29 +0100

    removed environment configuration.

---
+++ scripts/syncdb.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -a
+source .env
+
+
+ssh web-user@${REMOTE_HOST} "cd ${REMOTE_HOST_PATH}; magerun db:dump /tmp/db-dump.gz --compression=gz --strip=\"@stripped\""
+scp web-user@${REMOTE_HOST}:/tmp/db-dump.gz ./container_configuration/db/dump/db-dump.gz
