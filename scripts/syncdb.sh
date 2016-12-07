591ec2a089338f0f88fa2dd55629a750222327ce
Don Bosco van Hoi <vanhoi@mothership.de>
2016-12-07 13:39:06 +0100

    Fixed dump

--- scripts/syncdb.sh
+++ scripts/syncdb.sh
@@ -5,4 +5,4 @@ source .env
 
 
 ssh web-user@${REMOTE_HOST} "cd ${REMOTE_HOST_PATH}; magerun db:dump /tmp/db-dump.gz --compression=gz --add-routines --strip=\"@stripped\""
-scp web-user@${REMOTE_HOST}:/tmp/db-dump.gz ./container_configuration/db/dump/db-dump.gz
+scp web-user@${REMOTE_HOST}:/tmp/db-dump.gz ./container_configuration/db/dump/db-dump.sql.gz
