9654f387268a52d60dd0a02b3ba9c93d3e9bead3
Don Bosco van Hoi <vanhoi@mothership.de>
2016-12-07 13:36:32 +0100

    added routines

--- scripts/syncdb.sh
+++ scripts/syncdb.sh
@@ -4,5 +4,5 @@ set -a
 source .env
 
 
-ssh web-user@${REMOTE_HOST} "cd ${REMOTE_HOST_PATH}; magerun db:dump /tmp/db-dump.gz --compression=gz --strip=\"@stripped\""
+ssh web-user@${REMOTE_HOST} "cd ${REMOTE_HOST_PATH}; magerun db:dump /tmp/db-dump.gz --compression=gz --add-routines --strip=\"@stripped\""
 scp web-user@${REMOTE_HOST}:/tmp/db-dump.gz ./container_configuration/db/dump/db-dump.gz
