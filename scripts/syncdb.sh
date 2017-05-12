e741a81980afa40a5c2e27f4e3215d5b62bdb740
Don Bosco van Hoi <vanhoi@mothership.de>
2017-05-12 11:33:04 +0200

    EXX-1 Removed original Amazon Extension. Replaced existing creative style extension with new one.

--- scripts/syncdb.sh
+++ scripts/syncdb.sh
@@ -3,6 +3,8 @@
 set -a
 source .env
 
+# development @log catalog_product_entity catalog_product_entity_*
+# magerun db:dump --strip="@development @log catalog_product* catalog_category_product core_url_rewrite cataloginventory_stock_status* cataloginventory_stock_item" --add-routines --compression=gz /tmp/db-dump.gz
 
-ssh web-user@${REMOTE_HOST} "cd ${REMOTE_HOST_PATH}; magerun db:dump /tmp/db-dump.gz --compression=gz --add-routines --strip=\"@stripped\""
+# ssh web-user@${REMOTE_HOST} "cd ${REMOTE_HOST_PATH}; magerun db:dump /tmp/db-dump.gz --compression=gz --add-routines --strip=\"@stripped\""
 scp web-user@${REMOTE_HOST}:/tmp/db-dump.gz ./container_configuration/db/dump/db-dump.sql.gz
